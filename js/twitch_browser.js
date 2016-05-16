6b1ec23801591d50d9fef9b42217fb035ea9ffd8
4iar <4iar@users.noreply.github.com>
2016-05-16 20:08:35 +0100

    Add basic module layout

---
+++ js/twitch_browser.js
@@ -0,0 +1,36 @@
+"use strict"
+
+angular.module('twitchBrowser', [])
+    .service('channelSubscriptionService', function() {
+        this.names = [];
+
+        this.addChannelNames = function(names) {
+            this.names = this.names.concat(names);
+        };
+
+        this.getChannelNames = function() {
+            return this.names;
+        };
+    })
+    .controller("channelSubscriptionManager", function($scope, channelSubscriptionService) {
+
+        var defaultChannelNames = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff"];
+        channelSubscriptionService.addChannelNames(defaultChannelNames);
+        
+        $scope.channels = channelSubscriptionService.getChannelNames();
+
+        $scope.testy = function () {
+            return 32;
+        };
+
+        $scope.addChannels = function addChannels(channel) {
+            channelSubscriptionService.addChannelNames(channel);
+        };
+        
+        $scope.deleteChannel = function (channel) {
+            return;
+        };
+    })
+    .controller("channelGrabber", function($scope, $http, channelSubscriptionService) {
+    });
+
