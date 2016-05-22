08d1133286edf1d3fc9e971628f213a3dc3eabf6
4iar <4iar@users.noreply.github.com>
2016-05-22 13:14:50 +0100

    Add tests channelGrabber.updateChannels tests

--- test/test_channelGrabber.js
+++ test/test_channelGrabber.js
@@ -62,21 +62,74 @@ describe('Controller: channelGrabber', function () {
     };
 
     describe('updateChannels', function () {
-        it('should return an array of objects that contains data about subscribed channels', function () {
-
-            var channelDataArray = scope.updateChannels();
-            expect(channelDataArray).toEqual(jasmine.any(Array));
-
-            for (var i = 0; i < channelDataArray.length; i++) {
-                // Check that all the objects in the array have the correct properties
-                expect(isChannelDataObject(channelDataArray[i])).toBeTruthy();
+        beforeEach(function () {
+            scope.parseChannelData = function (channelName) {
+                return {
+                    'name': 'storbeck',
+                    'url': 'https://www.twitch.tv/freecodecamp',
+                    'description': 'Local Weather Project - Nick LaBelle #programming',
+                    'online': true,
+                    'avatarUrl': 'https://static-cdn.jtvnw.net/jtv_user_pictures/storbeck-profile_image-7ab13c2f781b601d-300x300.jpeg'
+                };
             };
         });
-        
-        it('array length should correspond to the number of subscribed channels', function() {
-            var channelDataArray = scope.updateChannels();
+
+        xit('should set $scope.channels to an array of objects that contains data about subscribed channels', function () {
+            scope.updateChannels();
+            expect(scope.channels).toEqual(jasmine.any(Array));
+
+            // this is bad because test results are affected by length of the array --- see below test
+            // i.e. false +ve if array is not written
+            for (var i = 0; i < scope.channels.length; i++) {
+                // Check that all the objects in the array have the correct properties
+                expect(isChannelDataObject(scope.channels[i])).toBeTruthy();
+            };
+        });
+
+
+        it('should call $http.get() with the correct urls', function () {
+
+            $httpBackend.expectGET('https://api.twitch.tv/kraken/channels/freecodecamp');
+            $httpBackend.expectGET('https://api.twitch.tv/kraken/channels/storbeck');
+            $httpBackend.expectGET('https://api.twitch.tv/kraken/channels/brunofin');
+            $httpBackend.expectGET('https://api.twitch.tv/kraken/channels/channelthatdoesnotexist');
+
+            scope.updateChannels();
+
+            $httpBackend.flush()
+        });
+
+        xit('should call $scope.parseChannelData() with the response.data', function () {
+            spyOn(scope, 'parseChannelData').and.callThrough();
+
+            scope.updateChannels();
+
+            // TODO: fix this test
+            // .calls.allArgs() doesn't seem to show any arguments when called from the $http.get callback
+            // but does work when called synchronously e.g. in the for loop
+            expect(scope.parseChannelData.calls.allArgs()).toEqual(
+                ['{"mature":false,"status":"Local Weather Project - Nick LaBelle #programming","broadcaster_language":"en","display_name":"FreeCodeCamp","game":"Creative","language":"en","_id":79776140,"name":"freecodecamp","created_at":"2015-01-14T03:36:47Z","updated_at":"2016-05-20T13:00:58Z","delay":null,"logo":"https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-profile_image-d9514f2df0962329-300x300.png","banner":null,"video_banner":"https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-channel_offline_image-b8e133c78cd51cb0-1920x1080.png","background":null,"profile_banner":"https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-profile_banner-6f5e3445ff474aec-480.png","profile_banner_background_color":null,"partner":false,"url":"https://www.twitch.tv/freecodecamp","views":148163,"followers":9674,"_links":{"self":"https://api.twitch.tv/kraken/channels/freecodecamp","follows":"https://api.twitch.tv/kraken/channels/freecodecamp/follows","commercial":"https://api.twitch.tv/kraken/channels/freecodecamp/commercial","stream_key":"https://api.twitch.tv/kraken/channels/freecodecamp/stream_key","chat":"https://api.twitch.tv/kraken/chat/freecodecamp","features":"https://api.twitch.tv/kraken/channels/freecodecamp/features","subscriptions":"https://api.twitch.tv/kraken/channels/freecodecamp/subscriptions","editors":"https://api.twitch.tv/kraken/channels/freecodecamp/editors","teams":"https://api.twitch.tv/kraken/channels/freecodecamp/teams","videos":"https://api.twitch.tv/kraken/channels/freecodecamp/videos"}'],
+                ['{"mature":null,"status":null,"broadcaster_language":null,"display_name":"storbeck","game":null,"language":"en","_id":86238744,"name":"storbeck","created_at":"2015-03-25T02:23:40Z","updated_at":"2016-05-20T14:00:35Z","delay":null,"logo":"https://static-cdn.jtvnw.net/jtv_user_pictures/storbeck-profile_image-7ab13c2f781b601d-300x300.jpeg","banner":null,"video_banner":null,"background":null,"profile_banner":null,"profile_banner_background_color":null,"partner":false,"url":"https://www.twitch.tv/storbeck","views":567,"followers":9,"_links":{"self":"https://api.twitch.tv/kraken/channels/storbeck","follows":"https://api.twitch.tv/kraken/channels/storbeck/follows","commercial":"https://api.twitch.tv/kraken/channels/storbeck/commercial","stream_key":"https://api.twitch.tv/kraken/channels/storbeck/stream_key","chat":"https://api.twitch.tv/kraken/chat/storbeck","features":"https://api.twitch.tv/kraken/channels/storbeck/features","subscriptions":"https://api.twitch.tv/kraken/channels/storbeck/subscriptions","editors":"https://api.twitch.tv/kraken/channels/storbeck/editors","teams":"https://api.twitch.tv/kraken/channels/storbeck/teams","videos":"https://api.twitch.tv/kraken/channels/storbeck/videos"}'],
+                ['{"error":"Unprocessable Entity","status":422,"message":"Channel \'brunofin\' is not available on Twitch"}'],
+                ['{"error":"Not Found","status":404,"message":"Channel \'channelthatdoesnotexist\' does not exist"}']
+            );
+        });
+
+        xit('should push return values from $scope.parseChannelData() to $scope.channels', function () {
+            spyOn(scope, 'parseChannelData').and.callThrough();
+
+            var channelObject = {
+                'name': 'storbeck',
+                'url': 'https://www.twitch.tv/freecodecamp',
+                'description': 'Local Weather Project - Nick LaBelle #programming',
+                'online': true,
+                'avatarUrl': 'https://static-cdn.jtvnw.net/jtv_user_pictures/storbeck-profile_image-7ab13c2f781b601d-300x300.jpeg'
+            };
+
+            // TODO: same as above
+            scope.updateChannels();
             
-            expect(scope.channels).toEqual(23);
+            expect(scope.channels).toEqual([channelObject, channelObject, channelObject, channelObject]);
         });
     });
 
